f8b003b7534a6e5362482ace1419b9cac10f78f8
Jason Frame <jason@onehackoranother.com>
2014-03-25 15:27:07 +0000

    tabs -> spaces

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
 var floor = Math.floor;
 
 function clamp(min, max, v) {
-	if (v < min) return min;
-	if (v > max) return max;
-	return v;
+    if (v < min) return min;
+    if (v > max) return max;
+    return v;
 }
 
 module.exports = function(audioContext, opts) {
 
-	opts = opts || {};
+    opts = opts || {};
 
-	var bufferSize 		= opts.bufferSize || 4096,
-		channelCount	= opts.channelCount || 2,
-		bits 			= clamp(1, Infinity, (opts.bits || 8) | 0),
-		normFreq		= clamp(0, 1, opts.frequency || 1),
-		step 			= 2 * Math.pow(0.5, bits),
-		invStep			= 1 / step,
-		phasor 			= 0,
-		last 			= 0;
+    var bufferSize      = opts.bufferSize || 4096,
+        channelCount    = opts.channelCount || 2,
+        bits            = clamp(1, Infinity, (opts.bits || 8) | 0),
+        normFreq        = clamp(0, 1, opts.frequency || 1),
+        step            = 2 * Math.pow(0.5, bits),
+        invStep         = 1 / step,
+        phasor          = 0,
+        last            = 0;
 
-	var processor = audioContext.createScriptProcessor(bufferSize, channelCount, channelCount);
-		
-	processor.onaudioprocess = function(evt) {
+    var processor = audioContext.createScriptProcessor(bufferSize, channelCount, channelCount);
+        
+    processor.onaudioprocess = function(evt) {
 
-		var ib = evt.inputBuffer,
-			ob = evt.outputBuffer;
+        var ib = evt.inputBuffer,
+            ob = evt.outputBuffer;
 
-		for (var i = 0; i < channelCount; ++i) {
+        for (var i = 0; i < channelCount; ++i) {
 
-			var id = ib.getChannelData(i),
-				od = ob.getChannelData(i);
+            var id = ib.getChannelData(i),
+                od = ob.getChannelData(i);
 
-			for (var s = 0, l = ob.length; s < l; ++s) {
-				phasor += normFreq;
-				if (phasor > 1) {
-					phasor -= 1;
-					last = step * floor((id[s] * invStep) + 0.5);
-				}
-				od[s] = last;
-			}
+            for (var s = 0, l = ob.length; s < l; ++s) {
+                phasor += normFreq;
+                if (phasor > 1) {
+                    phasor -= 1;
+                    last = step * floor((id[s] * invStep) + 0.5);
+                }
+                od[s] = last;
+            }
 
-		}
+        }
 
-	}
+    }
 
-	return processor;
+    return processor;
 
 }
